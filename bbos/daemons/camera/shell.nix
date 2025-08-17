501e8d20baf20ea8d4cf5f726d291591357c00d2
Raghava Uppuluri <raghava.upp13@gmail.com>
2025-08-17 17:49:11 +0000

    pin nix hash

--- bbos/daemons/camera/shell.nix
+++ bbos/daemons/camera/shell.nix
@@ -1,5 +1,8 @@
 let
-pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/63dacb46bf939521bdc93981b4cbb7ecb58427a0.tar.gz") {};
+pkgs = import (fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/63dacb46bf939521bdc93981b4cbb7ecb58427a0.tar.gz";
+  sha256 = "sha256:1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
+}) {};
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [ 
